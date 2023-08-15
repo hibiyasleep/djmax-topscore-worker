b76b8276f3d40e6d6c6e94ff63d819aa3252217d
hibiya (김종이) <inemuri@hibiya.moe>
2023-08-16 02:55:48 +0900

    update alias

--- src/alias.js
+++ src/alias.js
@@ -1,12 +1,12 @@
-const LINES = `
-제목
+const LINES = `!!New Game Start!!	뉴겜스
 #mine (feat. Riho Iwamoto)
 공성전 ~Pierre Blanche, Yonce Remix~
 광명의 루 : 루 라바다
 너로 피어오라
 너로피어오라
+피어오라	너피오
 모짜르트 교향곡 40번 1악장
-바람에게 부탁해 ~Live Mix~
+바람에게 부탁해 ~Live Mix~	바부라
 복수혈전
 부여성 ~Blosso Remix~
 비상 ~Stay With Me~
@@ -14,146 +14,167 @@ const LINES = `
 연합군과 제국군
 염라
 전설이 시작된 곳 ~Void Rover Remix~	전시곳
-죽음의 신 : 크로우 크루아흐
+죽음의 신 : 크로우 크루아흐	죽신
 최종무곡
 카트라이더 Mashup ~Cosmograph Remix~	코스모
 카트라이더 Mashup ~Pure 100% Remix~	퓨백
 카트라이더, 크레이지아케이드, 버블파이터 Main theme ~CHUCK Remix~	척	척리믹	배찌
-피아노 협주곡 1번
-AD2222
+피아노 협주곡 1번	피협
+A Site De La Rue	데라루
+AD2222	ㅁㅇ2222
 Airlock	에어락
-Airwave	에어웨이브
+Airwave	에어웨이브	에웨
 ALiCE	앨리스
-ANALYS
-Angelic Sphere
-Angelic Tears
+ANALYS	아날리스
+Angel	엔젤
+Angelic Sphere	엔젤릭스피어
+Angelic Tears	엔젤릭티어즈
 ANiMA	아니마
-Astro Fight
-Attack
-Aurora Borealis
-AXION
+Aquaris	아쿠아리스
+Astro Fight	아스트로	아스트로파이트
+Attack	어택
+Aurora Borealis	오로라	보레알리스
+AXION	악시온
+Back to the oldschool	백올스	오수	오스	osu
 Bamboo on Bamboo	밤부
 Barbarous Funera
-Beat U Down
-BEE-U-TIFUL
-Beyond the Future
+Beat U Down	비추다운
+BEE-U-TIFUL	뷰티풀
+Beyond the Future	비욘드더퓨처	비욘더퓨처
 Beyond Yourself
-Binary World
+Binary World	바이너리월드
 BLACK GOLD	흑금
 Black MInD	블랙마인드	블마
-Black Swan
-BlythE
-Boom!
-Brain Storm	브톰
-Break a Spell
-Break!
-Bright Future
-Chain of Gravity	체오그
-Chemical Slave	케이컬
+Black Swan	블랙스완
+BlythE	블라이스
+Boom!	붐
+Brain Storm	브레인스톰	브톰
+Break a Spell	브레이크어스펠
+Break!	브레이크
+Bright Future	브퓨
+Burn it Down	번잇다운
+Celestial Tears	셀레스티얼티어즈
+Chain of Gravity	체인오브그래비티	체오그
+Chemical Slave	케미컬슬레이브	케미컬
 Child of Night	차오나
-Chrysanthemum
+Chrysanthemum	크리산세멈
 Cocked Pistol	콕피
 CODE NAME : ZERO	코넴제
-Color
-Comet Coaster	코멧	코멧코스터
-conflict	컨플릭트	즈오르히
+Color	컬러
+Colours of Sorrow
+Comet Coaster	코멧코스터	코멧
+Complex	컴플렉스	콤플렉스
+conflict	컨플릭트	컨플
 Cthugha	크투가
-Cyberozar
+Cyberozar	삭제
 Cypher Gate	사게	싸게
 D2	디투
-Dance of the Dead
+Dance of the Dead	댄오데
 Dancing Planet	댄싱플래닛	댄플
-DARK ENVY
-Dark Prism
-DataErr0r	데이터에러
-Daydream
-Daylight
+DARK ENVY	다크엔비
+Dark Lighting	다크라이트닝	닼라
+Dark Prism	다크프리즘	닼프
+DataErr0r	데이터에러	데에
+Daydream	데이드림
+Daylight	데이라잇	데이라이트	데라
+Deadly Bomber	데들리봄버
 Deborah	데보라
+Desperado	데스페라도
+DIE IN	다인	다이인	아근다
 Disappearing Act	퇴근	퇴근행동
 Don't Die	돈다이
 Dreadnought	드레드노트
-Dream it
+Dream it	드림잇
 Dream of Winds	드오윈
 Dream of you	드오유
 Dual Strikers	듀스
 Dysthymia	디스티미아
-Electronics
-EMber
-Emblem
-Emerge
-Enemy Storm ~Dark Jungle Mix~
+Electronics	일렉트로닉스
+EMber	엠버
+Emblem	엠블렘
+Emerge	이머지
+Enemy Storm ~Dark Jungle Mix~	에너미스톰
 ENERGY SYNERGY MATRIX	esm
 Enter The Universe	우주입갤
 Entrance	엔트런스
 Extreme Z4
-Feel Ma Beat
-Fermion
-Festa Nova
-FIGHT NIGHT
+Feel Ma Beat	필마빗
+Fermion	페르미온
+Festa Nova	페스타노바
+FIGHT NIGHT	파이트나이트	파잇나잇
 For Seasons ~Air Guitar Mix~	포시즌
-For the IKARUS
-Forgotten
-Frontline
-Fundamental
-Fury
+For the IKARUS	이카루스
+Forgotten	포가튼	폴가튼	포거튼	폴거튼
+From Hell to Breakfast	아침밥
+Frontline	소전
+Fundamental	펀더멘털	펀다멘털	푼더멘털	푼다멘털	펀더멘탈	펀다멘탈	푼더멘탈	푼다멘탈
+Fury	퓨리
 Garakuta Doll Play	가라쿠타
-glory dat -JHS Remix-	glory day -JHS Remix-
+-JHS Remix-	glory day -JHS Remix-
 glory day (Mintorment Remix)
-Gone Astray
+Gloxinia	글록시니아
+Gone Astray	곤아스
 Good Night, Bad Luck
 Got more raves?	갓모레
-Grave Consequence
-Grid System
+Grave Consequence	그컨
+Grid System	그리드시스템
 Groove Prayer
 HAMSIN	햄신
-Hanz up!
+Hanz up!	핸즈업
 Heart of Witch	하오위
 HELIX	헬릭스
+Hell'o	헬오	헬로	헬
 HEXAD	헥사드
-Higher
+Higher	하이어
 Holy Orders (Be Just Or Be Dead)
-I want You ~반짝 반짝 Sunshine~
+Hyper Drive	하이퍼드라이브	하드
+Hypernaid	하이퍼네이드
+I want You ~반짝 반짝 Sunshine~	아원츄
+I'M ALIVE	암얼라이브	암얼라
 I've got a feeling
 Ikazuchi	이카즈치
-Imaginary dance
-In my Dream
+Imaginary dance	상상춤
+In my Dream	인마이드림
 It's my war now
-JBG
-Kal_wrnw
+JBG	지방간
+Kal_wrnw	마지죽
 Kensei	켄세이	겐세이
-Keys to the World
+Keys to the World	키투월
 KICK IT	킥잇
 KILLER BEE	킬러비
-Knowledge System
-KUDA
-Kung Brother
+Knowledge System	널리지시스템	널리지
+KUDA	쿠다
+Kung Brother	쿵
 L	ㄴ
-La Campanella : Nu Rave	라캄	라캄파넬라
-Landscape
-Leave me alone
-Lemonade
-Liar
-Light House
-Lights of Muse	라오뮤
+La Campanella : Nu Rave	라캄파넬라	라캄
+Landscape	랜드스케이프
+Leave me alone	립미얼론
+Lemonade	레몬에이드
+Liar	라이어
+Light House	라이트하우스	라잇하우스	라하
+Lights of Muse	라이츠오브뮤즈	라이트오브뮤즈	라오뮤
+LIMBO	림보
 Lisrim	리즐림	리슬림
-Lost Temple
+Lost Temple	로스트템플	로템
+Love.Game.Money	럽겜머니	럽겜머
 Mammal	마말
-Marionette
-Marry me, Nightmare
-MASAI
-Melody
-Melonaid
+Marionette	마리오네트
+Marry me, Nightmare	메리미
+MASAI	마사이
+Melody	멜로디
+Melonaid	멜론에이드	멜로네이드
 Mess it Up
+Metagalactic	메타갈락틱	메타갤럭틱	메갈
 Midnight Blood
 Mind Control	마컨	마인드컨트롤
-Minimal Life
+Minimal Life	미니멀
 Minus 3	마삼
 Misty E'ra 'Mui'	무이
 Moment (feat. Nauts)
-Mr.Lonely
-Mulch
-MUSEDASH!!!!
-Myosotis
+Mr.Lonely	미스터론리	론리
+Mulch	멀치	물치
+MUSEDASH!!!!	뮤대
+Myosotis	마이오소티스	물망초
 NB Girls
 NB POWER
 NB RANGER
@@ -162,95 +183,113 @@ NB RANGER -Virgin Force-
 NB RANGERS - 운명의 Destiny	데스티니
 NB Rangers -Returns-
 Never Die	네버다이
-Never let you go
+Never let you go	네버렛츄고
 Nevermind	네버마인드
-Nightmare	나메
-Obelisque	오벨리스크
-OBLIVION
-OBLIVION ~Rockin' Night Style~
-Odysseus	오디	오디세우스	디세우스
-Old Gold
+New World	뉴월드
+Nightmare	나이트메어	나메
+Nih	니힐리즘	니힐
+Obelisque	오벨리스크	오벨
+OBLIVION	오빌리비언	오빌
+OBLIVION ~Rockin' Night Style~	오빌락
+Odysseus	오디세우스	오디	디세우스
+Old Gold	올드골드	올골
 OrBiTal	오비탈
 ouroboros -twin stroke of the end-	우로보로스
 Out Law : Reborn	아웃로
-Out of CTRL
+Out of CTRL	아오컨
 Over Me	오버미
-OVER THE NIGHT
-Over the Rainbow
-Para-Q
+OVER THE NIGHT	오버더나잇	오버더나이트
+Over the Rainbow	오버더레인보우
+Para-Q	파라큐
 Plasma Sphere	플스
 PUPA	푸파	퓨파	나비
-quixotic
+Puzzler
+quixotic	퀵소틱
 Rage Of Demon	레오데
 Ray of Illuminati
-Ray Tuning
+Ray Tuning	레이튜닝
 Real Over Drive
-Red Eyes
-Remains Of Doom
-Remember Me
-Renovation
-Retention
-Right Back
+Red Eyes	레드아이즈
+Remains Of Doom	레오둠
+Remember Me	리멤버미
+Renovation	리노베이션
+Retention	리텐션
+Right Back	라잇백
 Rising The Sonic
 Road Of Death	로오데
 Rock Or Die
 Rolling On the Duck
-Royal Clown
-Runaway
+Royal Clown	로열크라운	로얄크라운
+Runaway	런어웨이
 Ruti'n (GOTH Wild Electro Remix)	루틴리믹
-Secret Dejavu
-Seeker
-Set Me Free	셋미프리
-Showdown
-SigNalize
+Sand Storm	샌드스톰
+Secret Dejavu	시크릿데자뷰
+Seeker	시커
+Set Me Free	셋미프리	셋미
+Showdown	쇼다운
+Showdown (ez2on)
+SigNalize	시그널라이즈
 Silent Clarity
-SIN
-SIN ~The Last Scene~
+SIN	신	냐ㅜ
+SIN ~The Last Scene~	신더라신
 SON OF SUN	추장
-Space Challenger
-Space of Soul
-SQUEEZE
-STALKER
-Start The Adventure ~SOPHI Remix~
-STEP
+Space Challenger	스챌
+Space of Soul	스오소
+Sparrow	스패로우	김도
+SQUEEZE	스퀴즈
+STALKER	스토커
+Start The Adventure ~SOPHI Remix~	메
+Stay	스테이
+Stay Alive	스테이얼라이브	스얼
+STEP	스텝
+Stolen Memory	스톨른메모리	스톨른
 Super lovely	슈럽
 Supernova	슈퍼노바
-Supersonic 2011
-Syriana ~Blast Wave Mix~
-Techno Racer
-The Clear Blue Sky
-The Feelings
-The Guilty
+SuperSonic	슈퍼소닉	두부소닉	슈스
+Supersonic 2011	슈퍼소닉2011	슈스2011
+Syriana ~Blast Wave Mix~	시리아나
+Techno Racer	테크노레이서
+The Clear Blue Sky	클블스
+The Feelings	필링
+The Four Seasons : Summer 2017	사계	하농
+The Guilty	길티
 The Obliterator	오빌
-The One
+The One	더원
 The Rain Maker	레인메이커
-The wheel to the right
+The wheel to the right	휠투라	인도인
 Thor	토르
 Tic! Tac! Toe!	틱택토
 Trrrickster	트릭스터
-U.A.D
-Urban Night
-v o l d e n u i t	볼데누잇	voldenuit
-Ventilator	벤틸
-Vertical Floating
-Vile Requiem
+Trrrickster!!
+Trrricksters!!
+U.A.D	UAD	uad	울트라아줌마댄스
+Urban Night	어반나이트
+v o l d e n u i t	볼데누잇	voldenuit	볼데
+Ventilator	벤틸레이터	벤틸
+Vertical Eclipse	태그	버클립스
+Vertical Floating	버티컬플로팅	버플
+Vile Requiem	바일레퀴엠
 VORTEX
-Waiting for you
+Waiting for you	댕댕포유
 Warrior	워리어
 Watch Your Step	발조심
 We're All Gonna Die	고너다이	거너다이	고나다이
+Weaponize	웨포나이즈
+WEIRD WAVE	이뭔씨
 What am I fighting for?
 WhiteBlue	화블	화이트블루
 Winners	위너	위너즈
 Won't Back Down	원백다운	원백당
-WONDER $LOT 777
+WONDER $LOT 777	원더슬롯
 Xeus	제우스	째우스
-XING
+XING	싱
 XLASHER	슬래셔
+Y (CE Style)
 Yo Creo Que Si ~Live House Version~
 Your Own Miracle
-Zero to the hunnit
+Zero to the hunnit	제로투
 Zero-Break	제브
+Zeroize	제로아이즈
 ZET
 ZET ~Mr.Funky Remix~
 `
